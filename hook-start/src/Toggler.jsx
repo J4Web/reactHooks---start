1d8db518181e5b33642030002c046134c3e3db95
prakhar-glitch <54439226+prakhar-glitch@users.noreply.github.com>
2022-05-04 22:54:21 +0530

    second toggler

--- hook-start/src/Toggler.jsx
+++ hook-start/src/Toggler.jsx
@@ -2,16 +2,25 @@ import React,{useState} from 'react'
 
 function Toggler() {
     const [isHappy,setIsHappy]=useState(false);
+    //adding second toggler here
+    const [isApple, setIsApple]= useState(true);
     const setHappy=()=>
     {
 
         setIsHappy(!isHappy);
     }
+    const setFruit=()=>
+    {
+        setIsApple(!isApple);
+    }
   return (
     <div><h1 onClick={setHappy}>
     {isHappy? '😂': '😬'}
 
-    </h1></div>
+    </h1>
+    <h2 onClick={setFruit}>
+        {isApple? '🍎': '🍌'}
+    </h2></div>
   )
 }
 
